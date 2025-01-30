23f0bfe408a8ad711c49e7952167697ee2ef3856
Salma WADOUACHI <wadouachisalma@gmail.com>
2025-01-30 15:11:43 +0100

    feat: recuperation des données des projets

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ContactForm from "./ContactForm";
 import ContactInfo from "./ContactInfo";
+import SectionHeading from "../Helper/SectionHeading";
 
 const Contact = () => {
   return (
     <div className="pt-16 pb-16 ">
+      <div className="mt-22 mb-24">
+        <SectionHeading>CONTACTS</SectionHeading>
+      </div>
+
       <div className="grid grid-cols-2 w-[90%] mx-auto items-center ml-[10%] gap-24 mt-10 ">
         {/** CONTACT FORM */}
         <div>
